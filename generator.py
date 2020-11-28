fed3a2b8b155d54e4a50dd90ee26ccd91a497b26
VolodyaZAVR <62401307+VolodyaZAVR@users.noreply.github.com>
2020-11-29 02:59:28 +0300

    Update generator.py
    remove string "b=..."

--- generator.py
+++ generator.py
@@ -6,9 +6,9 @@ def generate(x0, y0, k, s, alpha, n):
     y = y0
     key = [0 for i in range(n)]
     for i in range(n):
-        b = int(math.fmod(abs(int(x * 10 ** 9)), 2))
         xn = math.cos(alpha) * x + math.sin(alpha) * (y + k * s * math.sin(x))
         xn = math.fmod(xn, 1)
+        b = int(math.fmod(abs(int(xn * 10 ** 9)), 2))
         yn = -math.sin(alpha) * x + math.cos(alpha) * (y + k * s * math.sin(x)) + (1 - s) * k * math.sin(xn)
         yn = math.fmod(yn, 1)
         x = xn
