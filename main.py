0a46697cc97bd4fb6b3674452a45dd2d40f00fa0
CyberSet <sashapiter2015@gmail.com>
2020-12-02 20:25:29 +0300

    output as saved image

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from generator import generate
 from encoder import encrypt_file
 from decoder import decode_file
+from input import toBinary
+from input import toData
+import imghdr
 import math
+
 
 if __name__ == "__main__":
     key = generate(0.5, 0.5, 3, 0.5, math.pi / 2, 128)
@@ -9,3 +13,14 @@ if __name__ == "__main__":
     encrypt_file(filename, key, 128)
     output_filename = "output.txt"
     decode_file(output_filename, key, 128)
+    filename = "input.png"
+    filetype = imghdr.what(filename)
+    binary = toBinary(filename)
+    key = generate(0.5, 0.5, 3, 0.5, math.pi / 2, len(binary))
+    binaryEncrypt = encrypt_file(binary, key)
+    outfile = open("encrypt.txt",'w')
+    for i in range(len(binaryEncrypt)):
+        outfile.write(str(binaryEncrypt[i]) + ' ')
+    outfile.close()
+    binaryDecode = decode_file(binaryEncrypt, key)
+    toData(binaryDecode,  filetype)
